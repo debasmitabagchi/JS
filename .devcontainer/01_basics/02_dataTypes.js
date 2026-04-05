904e3a92537d4894824708e8e43edca2fbe2f929
debasmitabagchi <144320102+debasmitabagchi@users.noreply.github.com>
2026-04-05 07:27:46 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version of js
+
+// alert(3+3) since we are using node.js not browser
+
+console.log(3+3) 
+console.log("Deb");
+// code readability should be high.
+
+let name = "debasmita"
+let age = 23
+let  LoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string=> ""
+// boolean=> true/false
+// null=> standalone value
+// undefines=>
+// symbol=>unique
+
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined); //undefined
+
