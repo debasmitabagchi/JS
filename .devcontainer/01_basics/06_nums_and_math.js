75f9ccfbd1dad366a9c41d979535a8607ac66173
debasmitabagchi <144320102+debasmitabagchi@users.noreply.github.com>
2026-04-06 16:29:25 +0000

    math and num

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.12538
+// console.log(otherNumber.toPrecision(3));
+
+const anotherNumber = 123.9786532
+// console.log(anotherNumber.toPrecision(3));
+
+const anotherNumber1 = 1123.9786532
+// console.log(anotherNumber1.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// *******************Maths****************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,7,9,3,6));
+// console.log(Math.max(4,7,9,3,6));
+
+// console.log(Math.random()); 0 to 1 ranged values
+console.log((Math.random()*10) + 1);
+console.log((Math.floor(Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
