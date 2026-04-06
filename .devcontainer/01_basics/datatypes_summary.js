05fc9c90d57a7f465dea0d56250ed3fbb369c8b3
debasmitabagchi <144320102+debasmitabagchi@users.noreply.github.com>
2026-04-06 05:04:21 +0000

    datatypes_summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,46 @@
+// Primitive datatype (Call by value)
+
+// 7 types : String, Number, Boolean. null, undefined,Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail ;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id == anotherId);
+
+const bigNumber = 9009237196482359327253715374235437n
+// console.log(bigNumber);
+
+
+// Reference Datatype (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Batman","Superman","Spiderman"] //array
+
+let myObj={
+    name :"Debasmita",                          //object
+    age:23,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof score);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof anotherId);
+console.log(typeof myObj);
+console.log(typeof heros);
+console.log(typeof myFunction);
